fe70c5df6404b32e184b451590ec0dabb4ad7b07
Christian R <christian.realubit@reevotech.com>
2023-02-23 15:00:55 +0800

    removed comments

--- new-sail-application/app/Http/Controllers/GameController.php
+++ new-sail-application/app/Http/Controllers/GameController.php
@@ -28,132 +28,26 @@ class GameController extends Controller
         $this->gameService = $gameService;
     }
 
-    // public function index() 
-    // {
-    //     //return "hello";
-
-    //     // $games = DB::table('games')->get();
-
-    //     // $games = Game::find(1521);
-    //     $games = Game::take(10)->get();
-    //     print_r($games);
-    // }
-
-    // public function index(IndexRequest $request): GameCollection
-    // {
-    //     // return new GameCollection($this->game->getFiltered($request->validated()));
-    //     return new GameCollection(Game::take(3)->get());
-    // }
-
-
-    public function indexx()
-    {
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'application/json'
-        // ])->get('https://aggr.reeltech.co/api/seamless/providerisb', ['data' => [
-        //     'api_password' => 'tabella-kingschance!',
-        //     'api_login' => 'tabella-kingschance-stage_mc_s',
-        //     'method' => 'getGameList',
-        //     'show_systems' => 0,
-        //     'currency' => "EUR",
-        // ]]);
-
-
-        // $client = new Client();
-        // $response = $client->get('https://aggr.reeltech.co/api/seamless/providerisb', [
-            // RequestOptions::JSON => [
-            //     'api_password' => 'tabella-kingschance!',
-            //     'api_login' => 'tabella-kingschance-stage_mc_s',
-            //     'method' => 'getGameList',
-            //     'show_systems' => 0,
-            //     'currency' => 'EUR',
-            //     ]
-        // ]);
-
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'application/json'
-        // ])->post('https://aggr.reeltech.co/api/seamless/providerisb', ["{
-        //     'api_password':'tabella-kingschance!',
-        //     'api_login':'tabella-kingschance-stage_mc_s',
-        //     'method':'getGameList',
-        //     'show_systems':0,
-        //     'currency':'EUR',
-        // }"]);
-
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'application/json'
-        // ])->post('https://aggr.reeltech.co/api/seamless/providerisb', [
-        //     'api_password' => 'tabella-kingschance!',
-        //     'api_login' => 'tabella-kingschance-stage_mc_s',
-        //     'method' => 'getGameList',
-        //     'show_systems' => 0,
-        //     'currency' => 'EUR',
-        //     ]);
-
-    
-        // $responseData = $response->json();
-        // dd($responseData);
-
-
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'application/json'
-        // ])->get('https://aggr.reeltech.co/api/seamless/providerisb', [
-        //     RequestOptions::JSON => [
-        //         'api_password' => 'tabella-kingschance!',
-        //         'api_login' => 'tabella-kingschance-stage_mc_s',
-        //         'method' => 'getGameList',
-        //         'show_systems' => 0,
-        //         'currency' => 'EUR',
-        //         ]
-        // ]);
-
-        // dd($response->body());
-        //print_r($response);
-        // return new GameCollection($response);
-    }
-
     public function index(IndexRequest $request, $id=null)
     {
-        // dd($id);
         if ($id) {
-            // $res = GameService::getGame($id);
-            // dd($res);
-            return GameService::getGame($id);
+            return $this->gameService->getGame($id);
         }
 
-
-        // print_r($request->filter);
-        
-        // return new GameCollection(GameService::getGameList());
-
         $games = GameService::getGameList();
-        // $gamesjd = json_decode($games);
-        // dd($gamesjd);
-
         $gamesCollection = collect($games['response']);
-        // dd($gamesCollection);
-
-        // $gamesCollection = collect($games->response);
 
         $start = 0;
         $max = 10;
         if (isset($request->filter) && Str::contains($request->filter, [':','page','provider','game'])) {
             
-
-            // get parameters
             $currentPage = null;
-
-            // print_r('current param: ' . $request->filter);
-
-
             if (str_contains($request->filter, "|")) {
                 list($filterBy, $currentPage) = explode("|", $request->filter);
             } else {
                 $filterBy = $request->filter;
             }
 
-
-            // $filter = $request->filter;
             list($key, $value) = explode(':', $filterBy);
             
             if ($key == 'provider') {
@@ -180,9 +74,6 @@ class GameController extends Controller
                 $filtered = $gamesCollection;
             }  
 
-
-            print_r('currentpage: ' . $currentPage);
-
             if ($currentPage) {
                 list($page, $pageVal) = explode(':', $currentPage);
                 if ($pageVal == 'all') {
@@ -200,11 +91,6 @@ class GameController extends Controller
         print_r('start: ' . $start);
 
         $chunked = $filtered->slice($start, $max);
-        
-        // $gamesCollection = collect($games->response);
-        // return $gamesCollection->all();
-        // dd($filtered->all());
-
         dd($chunked->all());
     }
 }
